ef2128eef81d12bb76e6aab8afa4264ecc058316
Alan Williams <pikethecrow@gmail.com>
2016-01-04 17:51:18 +0000

    Update chart.js

--- intScatterChart/chart.js
+++ intScatterChart/chart.js
@@ -11,13 +11,13 @@ var width = w - margin.left - margin.right;
 var height = h - margin.top - margin.bottom;
 
 var svg = d3.select("body").append("svg")
-			.attr("id", "chart")
-			.attr("width", w)
-			.attr("height", h);
+	.attr("id", "chart")
+	.attr("width", w)
+	.attr("height", h);
 
 var chart = svg.append("g")
-			.classed("display", true)
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.classed("display", true)
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 //create scales
 var colorScale = d3.scale.category10();
@@ -30,81 +30,83 @@ var y = d3.scale.linear()
           .domain([1, 5])
           .range([height, 0]);
 var responseScale = d3.scale.linear()
-											.domain(d3.extent(data, function(d) {
-												return d.responses;
-											}))
-											.range([2, 12]);
+		.domain(d3.extent(data, function(d) {
+			return d.responses;
+		}))
+		.range([2, 12]);
 
 //tickvalues
 var tickValues = [18, 25, 32, 39, 46, 53, 60, 67, 74];
 
 //declare Axis
 var xAxis = d3.svg.axis()
-							.scale(x)
-							.tickValues(tickValues)
-							.orient("bottom");
+		.scale(x)
+		.tickValues(tickValues)
+		.orient("bottom");
+		
 var yAxis = d3.svg.axis()
-							.scale(y)
-							.ticks(5)
-							.tickSize(20)
-							.tickFormat(function(d) {
-								return d.toFixed(1);
-							})
-							.orient("left");
+		.scale(y)
+		.ticks(5)
+		.tickSize(20)
+		.tickFormat(function(d) {
+			return d.toFixed(1);
+		})
+		.orient("left");
 
 //declare GridLines
 var xGridLines = d3.svg.axis()
-										.scale(x)
-										.tickValues(tickValues)
-										.tickSize(-height, -height)
-										.tickFormat("")
-										.orient("bottom");
+		.scale(x)
+		.tickValues(tickValues)
+		.tickSize(-height, -height)
+		.tickFormat("")
+		.orient("bottom");
+		
 var yGridLines = d3.svg.axis()
-										.scale(y)
-										.tickSize(-width, 0, 0)
-										.tickFormat("")
-										.orient("left");
+		.scale(y)
+		.tickSize(-width, 0, 0)
+		.tickFormat("")
+		.orient("left");
 
 //function to draw Axis
 function drawAxis(params) {
 	if(params.initialize) {
-			//gridlines
-			this.append("g")
-					.classed("gridline x", true)
-					.attr("transform", "translate(0, " + height + ")")
-					.call(params.axis.gridlines.x);
-			this.append("g")
-					.classed("gridline y", true)
-					.attr("transform", "translate(0, 0)")
-					.call(params.axis.gridlines.y);
-
-			//Axis
-			this.append("g")
-					.classed("axis x", true)
-					.attr("transform", "translate(0, " + height + ")")
-					.call(params.axis.x);
-			this.append("g")
-					.classed("axis y", true)
-					.attr("transform", "translate(0, 0)")
-					.call(params.axis.y);
-
-			this.select(".y.axis")
-					.append("text")
-					.classed("y axis-label", true)
-					.attr("transform", "translate(" + -56 + ", " + height/2 + ") rotate(-90)")
-					.text("Rating (1 = Low, 5 = High)");
-
-			this.select(".x.axis")
-					.append("text")
-					.classed("x axis-label", true)
-					.attr("transform", "translate(" + width/2 + ", " + 48 + ")")
-					.text("Customer Age");
-
-			this.append("g")
-					.append("text")
-					.classed("chart-header", true)
-					.attr("transform", "translate(0, -24)")
-					.text("");
+		//gridlines
+		this.append("g")
+			.classed("gridline x", true)
+			.attr("transform", "translate(0, " + height + ")")
+			.call(params.axis.gridlines.x);
+		this.append("g")
+			.classed("gridline y", true)
+			.attr("transform", "translate(0, 0)")
+			.call(params.axis.gridlines.y);
+	
+		//Axis
+		this.append("g")
+			.classed("axis x", true)
+			.attr("transform", "translate(0, " + height + ")")
+			.call(params.axis.x);
+		this.append("g")
+			.classed("axis y", true)
+			.attr("transform", "translate(0, 0)")
+			.call(params.axis.y);
+	
+		this.select(".y.axis")
+			.append("text")
+			.classed("y axis-label", true)
+			.attr("transform", "translate(" + -56 + ", " + height/2 + ") rotate(-90)")
+			.text("Rating (1 = Low, 5 = High)");
+	
+		this.select(".x.axis")
+			.append("text")
+			.classed("x axis-label", true)
+			.attr("transform", "translate(" + width/2 + ", " + 48 + ")")
+			.text("Customer Age");
+	
+		this.append("g")
+			.append("text")
+			.classed("chart-header", true)
+			.attr("transform", "translate(0, -24)")
+			.text("");
 	}
 }
 
@@ -121,29 +123,29 @@ function plot(params) {
 //setting a group for donuts
 	//enter the groups
 	this.selectAll(".donut")
-			.data(donuts)
-			.enter()
-				.append("g")
-				.attr("class", function(d) {
-					return d;
-				})
-				.classed("donut", true);
+		.data(donuts)
+		.enter()
+			.append("g")
+			.attr("class", function(d) {
+				return d;
+			})
+			.classed("donut", true);
 
-			//updating the groups
-			this.selectAll(".donut")
-					.style("fill", function(d, i) {
-						return colorScale(i);
-					})
-					.on("mouseover", function(d, i) {
-						d3.select(this)
-							.transition()
-							.style("opacity", 1)
-					})
-					.on("mouseout", function(d, i) {
-						d3.select(this)
-							.transition()
-							.style("opacity", 0.2)
-					});
+		//updating the groups
+		this.selectAll(".donut")
+				.style("fill", function(d, i) {
+					return colorScale(i);
+				})
+				.on("mouseover", function(d, i) {
+					d3.select(this)
+						.transition()
+						.style("opacity", 1)
+				})
+				.on("mouseout", function(d, i) {
+					d3.select(this)
+						.transition()
+						.style("opacity", 0.2)
+				});
 
 	//data plotting
 	donuts.forEach(function(donut) {
